4b9b775fcfd7ae680ef69627ba95d1e02e403975
lollipopfly <lollipop.fly@gmail.com>
2016-11-20 21:08:43 +0300

    make pagination page with phpQuery

--- pagination.php
+++ pagination.php
@@ -7,45 +7,47 @@
 </head>
 <body>
   <?php require 'scripts/pagination.php' ?>
-   <div class="container">
-     <div class="row">
-        <?php foreach ($items as $item): ?>
-         <div class="col-md-4">
-           <?php
-            $src = $item->children(0)->children(0)->children(0)->src;
-            $title = $item->children(0)->children(1)->children(0)->outertext;
-            $date = $item->children(0)->children(2)->outertext;
-           ?>
-           <?php if ($src): ?>
-            <img src="<?=$sitename.$src?>">
-           <?php endif ?>
+    <div class="container">
+      <div class="row">
+          <?php foreach ($items as $item): ?>
+           <div class="col-md-4">
+             <?php
+              $item = pq($item);
+              $src = $item->find('img')->attr('src');
+              $title = $item->find('h3')->html();
+              $date = $item->find('.meta')->html();
+             ?>
+             <?php if ($src): ?>
+              <img src="<?=$sitename.$src?>">
+             <?php endif ?>
 
-           <?php if ($title): ?>
-            <h2><?=$title ?></h2>
-           <?php endif ?>
+             <?php if ($title): ?>
+              <h2><?=$title ?></h2>
+             <?php endif ?>
 
-           <?php if ($date): ?>
-             <p><?=$date ?></p>
-           <?php endif ?>
-         </div>
-        <?php endforeach ?>
-     </div>
-     <?php if (($prev || $next) && $items): ?>
+             <?php if ($date): ?>
+               <p><?=$date ?></p>
+             <?php endif ?>
+           </div>
+          <?php endforeach ?>
+      </div>
+    <?php if (($prev || $next) && $items): ?>
        <div class="row">
          <div class="col-xs-12 text-right">
             <?php if ($prev): ?>
               <a href="/pagination.php?page=<?=$prev_page_id ?>">
-                <?=$prev->innertext ?>
+                <?=$prev->html() ?>
               </a>
             <?php endif ?>
             <?php if ($next): ?>
               <a href="/pagination.php?page=<?=$next_page_id ?>">
-                <?=$next->innertext ?>
+                <?=$next->html() ?>
               </a>
             <?php endif ?>
          </div>
        </div>
-     <?php endif ?>
+    <?php endif ?>
+    <?php phpQuery::unloadDocuments(); ?>
    </div>
 </body>
 </html>
--- scripts/pagination.php
+++ scripts/pagination.php
@@ -1,24 +1,27 @@
 <?php
-  require 'libs/simple_html_dom.php';
+  // require 'libs/simple_html_dom.php';
+  require 'libs/phpquery.php';
 
   $sitename = 'https://laravel-news.ru/';
   $page_id = 1;
   if(isset($_GET['page'])) $page_id = $_GET['page'];
 
   // Get page
-  $html = file_get_html('https://laravel-news.ru/?page=' . $page_id);
+  $html = file_get_contents('https://laravel-news.ru/?page=' . $page_id);
 
-  $items = $html->find('.blog-teaser');
-  $prev = $html->find('.older', 0);
-  $next = $html->find('.newer', 0);
+  phpQuery::newDocument($html);
 
-  if($prev) {
-    $prev_page_id = explode("page=", $prev->href);
+  $items = pq('.blog-teaser');
+  $prev = pq('.older');
+  $next = pq('.newer');
+
+  if($prev->html()) {
+    $prev_page_id = explode("page=", $prev->attr('href'));
     $prev_page_id = $prev_page_id[1];
   }
 
-  if($next) {
-    $next_page_id = explode("page=", $next->href);
+  if($next->html()) {
+    $next_page_id = explode("page=", $next->attr('href'));
     $next_page_id = $next_page_id[1];
   }
 ?>
